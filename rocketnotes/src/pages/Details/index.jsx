2767a78ed280627086cfadf38ce694dad9a23d63
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-12-11 17:52:10 -0300

    componentes e propriedades

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,13 +1,19 @@
 // import { Fragment } from "react"       <fragment>    
 
 import { Container } from './styles';
-
+import { Button } from "../../components/Button";
 export function Details(){//todo componente tem começar com a letra Maiuscula 
 
   return(
     <Container>
     <h1> Olá mundão </h1>
     <span>Bruno Eduardo</span>
+
+    <Button title="Entrar" loading />
+    <Button title="Cadastrar"/>
+    <Button title="Voltar" />
+    <Button title={10} />
+
     </Container>
   )
 }  
--- rocketnotes/src/styles/theme.js
+++ rocketnotes/src/styles/theme.js
@@ -4,6 +4,7 @@ export default {
         BACKGROUND_800: "#312E38",
         BACKGROUND_700: "#3E3B47",
 
+        WHITE: "#F4EDE8",
         ORANGE: "#FF9000",
 
         GRAY_100: "999591",
