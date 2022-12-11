199db2c0a302e139974812a544c5c46441d70946
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-12-11 15:35:23 -0300

    STYLES

---
+++ rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+// import { Fragment } from "react"       <fragment>    
+
+import { Container } from './styles';
+
+export function Details(){//todo componente tem começar com a letra Maiuscula 
+
+  return(
+    <Container>
+    <h1> Olá mundão </h1>
+    <span>Bruno Eduardo</span>
+    </Container>
+  )
+}  
