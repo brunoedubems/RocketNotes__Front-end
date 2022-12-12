3452b1c6c58a0da0337c5b7f2ced1d452b00fbbd
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-12-12 10:39:09 -0300

    Final Interface de Details

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,9 +1,9 @@
-// import { Fragment } from "react"       <fragment>    
-
-import { Container, Links } from './styles';
+ import { Container, Links,Content } from './styles';
+import { Tag } from "../../components/Tag";
 import { Header } from "../../components/Header";
-import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
 
 
 export function Details(){//todo componente tem começar com a letra Maiuscula 
@@ -11,6 +11,18 @@ export function Details(){//todo componente tem começar com a letra Maiuscula
   return(
     <Container>
     <Header/>
+
+    <main>
+    <Content>
+
+    <ButtonText title="Escluir nota"/>
+    <h1>Introdução ao React</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Autem voluptatum dignissimos animi alias ea maiores, atque, 
+       mollitia tempore ut similique suscipit eveniet velit illo fuga 
+       expedita a veniam distinctio dolorum.</p>
+
+
     <Section title="Links úteis">
       <Links>
         <li><a href="#">https://react-icons.github.io/react-icons/</a></li>
@@ -19,8 +31,14 @@ export function Details(){//todo componente tem começar com a letra Maiuscula
 
     </Section>
 
-    <Button title="Voltar" />
+    <Section title="Marcadores">
+      <Tag title="express"/>
+      <Tag title="nodejs"/>
+    </Section>
 
+    <Button title="Voltar" />
+    </Content>
+    </main>
     </Container>
   )
 }  
