002b8bdfe5c28e1b57dac4b0bd507436b33703cb
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-12-26 16:23:20 -0300

    Update index.jsx

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { TextArea } from '../../components/TextArea';
+
 import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section';
 import { Button } from '../../components/Button';
